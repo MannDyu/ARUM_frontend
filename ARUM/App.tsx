58b39ab0e806154d03d3ca4f3674398395983126
minsun24 <jms49526@gmail.com>
2024-07-18 01:32:37 +0900

    init screen

--- ARUM/App.tsx
+++ ARUM/App.tsx
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import MyPage from './src/screens/MyPage'
+import Home from './src/screens/Home'
+import Quest from './src/screens/Quest'
+import SelfTest from './src/screens/SelfTest'
+import Diary from './src/screens/Diary'
+import Community from './src/screens/Community'
+
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+  const Drawer = createDrawerNavigator();
+
+  function TabNavigator() {
+    return (
+      <Tab.Navigator
+        screenOptions={() => {
+          return { headerShown: false };
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Diary} />
+        <Tab.Screen name="MyPage" component={MyPage} />
+        <Tab.Screen name="Quest" component={Quest} />
+        <Tab.Screen name="SelfTest" component={SelfTest} />
+        <Tab.Screen name="Community" component={Community} />
+
+      </Tab.Navigator>
+    );
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      <Text>TEAM Manndyu's ARUManzim Project APP START!!! </Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+    //   <Text style={{backgroundColor:'yellow'}}>TEAM Manndyu's ARUManzim Project APP START!!! </Text>
+    //   <StatusBar style="auto" />
+    // </View>
+        <>
+        <StatusBar style="inverted" />
+        <NavigationContainer>
+          <Drawer.Navigator screenOptions={{ drawerPosition: "left" }}>
+            <Drawer.Screen
+              name="TabNavigator"
+              component={TabNavigator}
+              options={{ title: "Main" }}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ ARUM/src/screens/MyPage.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class MyPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MyPage</Text>
+      </View>
+    )
+  }
+}
