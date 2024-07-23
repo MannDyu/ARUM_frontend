742f78d64fbc61e6b74b18e0b6471d5e24a2f95e
Your Name <alex2321218@gmail.com>
2024-07-23 10:36:04 +0900

    refactor: Mission.tsx class to function component

--- ARUM/src/screens/Mission/Mission.tsx
+++ ARUM/src/screens/Mission/Mission.tsx
@@ -1,17 +1,17 @@
+import React from 'react';
 import { Text, View } from 'react-native';
-import React, { Component } from 'react';
 import DailyMission from './DailyMission';
-import SelectSection from './SelectSection';
+// import SelectSection from './SelectSection';
+import MissionPopup from './MissionPopUp';
 
-export default class Mission extends Component {
-  //! 네비게이션이 안 돼서 Mission.tsx자체에서 select section 렌더링
-  render() {
-    return (
-      <View>
-        <Text>Mission</Text>
-        <DailyMission /> 
-        {/* <SelectSection /> */}
-      </View>
-    )
-  }
-}
+const Mission = () => {
+  return (
+    <View>
+      <Text>Mission</Text>
+      <DailyMission />
+      {/* <SelectSection /> */}
+    </View>
+  );
+};
+
+export default Mission;
---
+++ ARUM/src/screens/Mission/MissionPopup.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MissionPopup = () => {
+
+}
+
+export default MissionPopup;
