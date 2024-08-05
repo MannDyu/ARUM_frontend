37b6655250bfc9bd82811099fca83d67da63b193
illudd <chominjung821@naver.com>
2024-08-05 19:36:53 +0900

    fix: homescreen add design

--- ARUM/src/screens/Home/Home.tsx
+++ ARUM/src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList, NavigationProp } from '../../navigation/types'; 
 
@@ -17,37 +17,70 @@ const HomeScreen = () => {
   const navigateToFindCenter = () => navigation.navigate('FindCenter');
 
   return (
-    <View style={styles.container}>
-      <View style={styles.grid}>
-        <TouchableOpacity style={styles.card} onPress={navigateToDiary}>
-          <Text style={styles.cardText}>감정일기 쓰기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.card} onPress={() => navigateToMission({ missionStatus: 'select' })}>
-          <Text style={styles.cardText}>일일 랜덤미션</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.card} onPress={navigateToSelfTest}>
-          <Text style={styles.cardText}>우울증 자가테스트</Text>
-          <View style={styles.redDot} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.card} onPress={navigateToFindCenter}>
-          <Text style={styles.cardText}>가까운 상담센터</Text>
-        </TouchableOpacity>
+    <ImageBackground 
+      source={require('../../assets/images/home/homemainCharacter.png')} 
+      style={styles.backgroundImage}
+      // resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <View style={styles.grid}>
+          <TouchableOpacity style={styles.card} onPress={navigateToDiary}>
+            <Image 
+              source={require('../../assets/images/home/diaryHomebutton.png')} 
+              style={styles.cardImage} 
+              resizeMode="contain"
+            />
+            <Text style={styles.cardText}>감정일기 쓰기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card} onPress={() => navigateToMission({ missionStatus: 'select' })}>
+            <Image 
+              source={require('../../assets/images/home/missionHomebutton.png')} 
+              style={styles.cardImage} 
+              resizeMode="contain"
+            />
+            <Text style={styles.cardText}>일일 랜덤미션</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card} onPress={navigateToSelfTest}>
+            <Image 
+              source={require('../../assets/images/home/selftestHomebutton.png')} 
+              style={styles.cardImage} 
+              resizeMode="contain"
+            />
+            <Text style={styles.cardText}>우울증 자가테스트</Text>
+            <View style={styles.redDot} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card} onPress={navigateToFindCenter}>
+            <Image 
+              source={require('../../assets/images/home/findcenterHomebutton.png')} 
+              style={styles.cardImage} 
+              resizeMode="contain"
+            />
+            <Text style={styles.cardText}>가까운 상담센터</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    width: '100%',
+    height: '100%',
+    
+  },
   container: {
     flex: 1,
     justifyContent: 'center', 
     alignItems: 'center',      
-    backgroundColor: '#FDFDED', 
-    paddingBottom: 1,        
+    backgroundColor: 'transparent',
+          
   },
   grid: {
     width: 330,
-    height: 140,
+    height: 30,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
@@ -59,15 +92,21 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     borderColor: 'black',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 9,
-    paddingBottom: 15,
     position: 'relative',
+    padding: 10,
+  },
+  cardImage: {
+    width: 60,  // 이미지 너비를 설정
+    height: 60,  // 이미지 높이를 설정
+    marginBottom: 10,
   },
   cardText: {
     fontSize: 19,
     color: 'black',
+    textAlign: 'center',
   },
   redDot: {
     width: 10,
@@ -81,3 +120,5 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
+
+
